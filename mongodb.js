620efc1b4fe410e8cbabd4db86d84433a8feef2f
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-14 21:44:05 +0530

    find which give the cursore

--- mongodb.js
+++ mongodb.js
@@ -9,11 +9,19 @@ MongoClient.connect(connectionUrl,{useNewUrlParser:true},(error,client)=>{
         return console.log('there is some problem to connect to database!');
     }
     const db=client.db(dataBaseName)
-    db.collection('myDatabase').findOne({completed:true},(err,user)=>{
-        if(err){
-            return console.log('there is some error occured during the find');
-        }
-        console.log(user);
+    // db.collection('myDatabase').findOne({completed:true},(err,user)=>{
+    //     if(err){
+    //         return console.log('there is some error occured during the find');
+    //     }
+    //     console.log(user);
+    // })
+    
+    // that give the cursore 
+    db.collection('myDatabase').find({completed:false}).count((err,count)=>{
+        console.log(count);
+    })
+    db.collection('myDatabase').find({completed:false}).toArray((err,users)=>{
+        console.log(users);
     })
 
     
