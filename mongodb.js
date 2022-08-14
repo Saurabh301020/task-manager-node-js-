7ee7b9c2bccd3930f95e22161ac763abaec6f848
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-14 21:30:05 +0530

    finde one method

--- mongodb.js
+++ mongodb.js
@@ -8,39 +8,15 @@ MongoClient.connect(connectionUrl,{useNewUrlParser:true},(error,client)=>{
     if(error){
         return console.log('there is some problem to connect to database!');
     }
-    // const db=client.db(dataBaseName)
-    // db.collection('companyUsers').insertOne({
-    //     name:'saurabh',
-    //     age:'22',
-    //     gender:'Male'
-    // },(error,result)=>{
-    //     if(error){
-    //         return console.log('there is some problem to inserting the doc.');
-    //     }
-    //     // console.log(result.ops);
-    // })
-
-    // challenge------
     const db=client.db(dataBaseName)
-    db.collection('myDatabase').insertMany([
-        {
-            description:'i need to finish the work',
-            completed:false
-        },
-        {
-            description:'i need to go park',
-            completed:true
-        },
-        {
-            description:'i need to go collage',
-            completed:false
-        }
-    ],(err,result)=>{
+    db.collection('myDatabase').findOne({completed:true},(err,user)=>{
         if(err){
-            return console.log('there is something error during the insertion!');
+            return console.log('there is some error occured during the find');
         }
-        
+        console.log(user);
     })
+
+    
 
 
     console.log('connected to database successfully!');
