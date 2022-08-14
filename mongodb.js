9be846ede241c61ab6e584889b1896d0320c0db5
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-14 17:14:45 +0530

    challege no.-1 in that we need to insert the many data!

--- mongodb.js
+++ mongodb.js
@@ -8,10 +8,40 @@ MongoClient.connect(connectionUrl,{useNewUrlParser:true},(error,client)=>{
     if(error){
         return console.log('there is some problem to connect to database!');
     }
+    // const db=client.db(dataBaseName)
+    // db.collection('companyUsers').insertOne({
+    //     name:'saurabh',
+    //     age:'22',
+    //     gender:'Male'
+    // },(error,result)=>{
+    //     if(error){
+    //         return console.log('there is some problem to inserting the doc.');
+    //     }
+    //     // console.log(result.ops);
+    // })
+
+    // challenge------
     const db=client.db(dataBaseName)
-    db.collection('companyUsers').insertOne({
-        name:'saurabh',
-        age:'22',
-        gender:'Male'})
+    db.collection('myDatabase').insertMany([
+        {
+            description:'i need to finish the work',
+            completed:false
+        },
+        {
+            description:'i need to go park',
+            completed:true
+        },
+        {
+            description:'i need to go collage',
+            completed:false
+        }
+    ],(err,result)=>{
+        if(err){
+            return console.log('there is something error during the insertion!');
+        }
+        
+    })
+
+
     console.log('connected to database successfully!');
 })
