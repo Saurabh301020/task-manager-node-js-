be13a45d684b2b9ae19d81460fa09c01f0bb2b6d
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-17 17:52:45 +0530

    setup the first api user

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,60 +1,6 @@
 const mongoose = require('mongoose')
-const validator=require('validator')
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser:true
-    // useCreateIndex:true
 })
 
-const User = mongoose.model('User',{
-    name:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    age:{
-        type:Number,
-        required:true,
-        default:0,
-        validate(value){
-            if(value<0){
-                throw new Error('Age must be +ve!')
-            }
-        }
-    },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid!')
-            }
-        }
-    },
-    password:{
-        type:String,
-        required:true,
-        minLength:7,
-        trim:true,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('pasword cannot contain password!')
-            }
-        }
-    }
-
-})
-
-const me = new User({
-    name:'saurabh',
-    age:22,
-    email:'saurabh@gmail.com',
-    password:'saurabh'
-})
-
-me.save().then(()=>{
-    console.log(me);
-}).catch((err)=>{
-    console.log(err);
-})
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('./db/mongoose')
+// that i used for running the mongoose------
+const User=require('./models/user')
+const app=express();
+
+app.use(express.json())
+app.post('/users',(req,res)=>{
+    const user=new User(req.body)
+    
+    user.save().then(()=>{
+        res.send(user)
+    }).catch((e)=>{
+        res.status(400).send(e)
+    })
+})
+
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log('server is start now...');
+})
