974cd20a978c5d6d070b328d7a8aec46956c2d40
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-30 14:17:43 +0530

    all task api completed

--- src/index.js
+++ src/index.js
@@ -16,3 +16,18 @@ const port = process.env.PORT || 3000
 app.listen(port,()=>{
     console.log('server is start now...');
 })
+//-----------------------------------------
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById("630b9636b41abc0fc17df0af")
+    // await task.populate('owner')
+    // console.log(task.owner)
+
+    // const user = await User.findById('630b8671e2f4bbfb5b9ba36c')
+    // await user.populate('tasks')
+    // console.log(user.tasks)
+}
+
+// main()
--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const { MongoTailableCursorError } = require('mongodb')
+const Task = require('./task')
 const userSchema = new mongoose.Schema(
     {
         name: {
@@ -51,6 +51,12 @@ const userSchema = new mongoose.Schema(
     }
 )
 
+userSchema.virtual('tasks',{
+    ref:'Task',
+    localField:'_id',
+    foreignField:'owner'
+})
+
 userSchema.methods.toJSON = function () {
     const user = this
     const userObject = user.toObject()
@@ -92,6 +98,11 @@ userSchema.pre('save', async function (next) {
 
 })
 
+userSchema.pre('remove',async function(next){
+    const user = this
+    await Task.deleteMany({owner:user._id}) 
+    next()
+})
 const User = mongoose.model('User', userSchema)
 
 module.exports = User;
